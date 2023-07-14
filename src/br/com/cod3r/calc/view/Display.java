8b8ad85d6cdeaa558a896db9ac2bfad219bef3fa
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-07-13 20:03:41 -0400

    a

--- src/br/com/cod3r/calc/view/Display.java
+++ src/br/com/cod3r/calc/view/Display.java
@@ -1,6 +1,10 @@
 package br.com.cod3r.calc.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Display extends JPanel {
+    public Display(){
+        setBackground(Color.BLACK);
+    }
 }
--- src/br/com/cod3r/calc/view/Keyboard.java
+++ src/br/com/cod3r/calc/view/Keyboard.java
@@ -1,6 +1,10 @@
 package br.com.cod3r.calc.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Keyboard extends JPanel {
+    public Keyboard(){
+        setBackground(Color.darkGray);
+    }
 }
