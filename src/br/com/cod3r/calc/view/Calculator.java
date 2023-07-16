b9fb0f20e7f9d38fb8e0626d41f1489855f3b578
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-07-16 07:23:00 -0400

    a

--- src/br/com/cod3r/calc/view/Calculator.java
+++ src/br/com/cod3r/calc/view/Calculator.java
@@ -1,14 +1,28 @@
 package br.com.cod3r.calc.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Calculator extends JFrame {
     public Calculator(){
+        organizarLayout();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(232,322);
         setLocationRelativeTo(null);
         setVisible(true);
     }
+
+    private void organizarLayout() {
+        setLayout(new BorderLayout());
+
+        Display display = new Display();
+        display.setPreferredSize(new Dimension(233, 60));
+        add(display, BorderLayout.NORTH);
+
+        Keyboard keyboard = new Keyboard();
+        add(keyboard, BorderLayout.CENTER);
+    }
+
     public static void main(String[] args) {
         new Calculator();
     }
--- src/br/com/cod3r/calc/view/Display.java
+++ src/br/com/cod3r/calc/view/Display.java
@@ -4,7 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Display extends JPanel {
+    private JLabel label;
     public Display(){
-        setBackground(Color.BLACK);
+        setBackground(Color.black);
+        label = new JLabel("1234,56");
+        label.setForeground(Color.white);
+        label.setFont(new Font("courier", Font.PLAIN,30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10,25));
+
+        add(label);
     }
 }
