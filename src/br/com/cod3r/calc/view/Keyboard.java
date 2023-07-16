89ae556f7a7fd8221328707656d0d7c1382308d8
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-07-16 17:11:41 -0400

    a

--- src/br/com/cod3r/calc/view/Keyboard.java
+++ src/br/com/cod3r/calc/view/Keyboard.java
@@ -8,26 +8,47 @@ public class Keyboard extends JPanel {
     private final Color dimGray = new  Color(105,105,105);
     private final Color darkBlue = new  Color(0,0,205);
     public Keyboard(){
-        setBackground(Color.darkGray);
-        setLayout(new GridLayout(5,4));
-        add(new CustomButton("AC", darkGray));
-        add(new CustomButton("+/-", darkGray));
-        add(new CustomButton("%", darkGray));
-        add(new CustomButton("/", darkBlue));
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
 
-        add(new CustomButton("7", dimGray));
-        add(new CustomButton("8", dimGray));
-        add(new CustomButton("9", dimGray));
-        add(new CustomButton("*", darkBlue));
+        setLayout(layout);
 
-        add(new CustomButton("4", dimGray));
-        add(new CustomButton("5", dimGray));
-        add(new CustomButton("6", dimGray));
-        add(new CustomButton("*", darkBlue));
+        c.weightx = 1;
+        c.weighty = 1;
+        c.fill = GridBagConstraints.BOTH;
 
-        add(new CustomButton("1", dimGray));
-        add(new CustomButton("2", dimGray));
-        add(new CustomButton("3", dimGray));
-        add(new CustomButton("*", darkBlue));
+        c.gridwidth = 2;
+        addButton("AC", darkGray, c, 0,0);
+        c.gridwidth = 1;
+        addButton("+/-", darkGray, c,2,0);
+        addButton("/", darkBlue, c,3,0);
+
+        addButton("7", dimGray, c,0,1);
+        addButton("8", dimGray, c,1,1);
+        addButton("9", dimGray, c,2,1);
+        addButton("*", darkBlue, c,3,1);
+
+        addButton("4", dimGray, c,0,2);
+        addButton("5", dimGray, c,1,2);
+        addButton("6", dimGray, c,2,2);
+        addButton("-", darkBlue, c,3,2);
+
+        addButton("1", dimGray, c,0,3);
+        addButton("2", dimGray, c,1,3);
+        addButton("3", dimGray, c,2,3);
+        addButton("+", darkBlue, c,3,3);
+
+        c.gridwidth = 2;
+        addButton("0", dimGray, c,0,4);
+        c.gridwidth = 1;
+        addButton(",", dimGray, c,2,4);
+        addButton("=", darkBlue, c,3,4);
+    }
+
+    private void addButton(String text, Color color, GridBagConstraints c, int x, int y) {
+        c.gridx = x;
+        c.gridy = y;
+        CustomButton customButton = new CustomButton(text,color);
+        add(customButton,c);
     }
 }
